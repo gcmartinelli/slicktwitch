fcca221540fbab08f40612f575848b1c89491929
gcmartinelli <gcmartinelli@gmail.com>
2019-07-28 05:26:22 +0000

    command line arguments added

--- README.md
+++ README.md
@@ -8,20 +8,32 @@ Only tested on Linux.
 
 Use at your own risk.
 
-#### Dependencies
+### Usage
+`-h | --help`												see help
+`-x | --nochat`												stream only mode
+`-n NICK | --nick=NICK`										set Twitch chat nick
+`-c CHANNEL | --channel=CHANNEL`							set Twitch channel name
+`-q QUALITY | --quality=QUALITY {best|worst|audio_only}`	set stream quality
+
+---
+
+### Dependencies
 * VLC Media Player - http://www.videolan.org/vlc
 * IRSSI - https://www.irssi.org
 
-#### Instalation
+### Instalation
 1. Clone with `git clone https://github.com/gcmartinelli/slicktwitch.git`
 2. Navigate to the directory where you cloned the script
 3. Launch with `./slicktwitch.sh`
 
 **Optional step**: Add `alias "slicktwitch=/path/to/slicktwitch.sh"` to your `.bashrc` file and launch with `slicktwitch` command.
 
+---
+
 #### Possible new features
-- [ ] Nick command line argument
-- [ ] Channel command line argument
+- [x] Nick command line argument
+- [x] Channel command line argument
+- [x] Stream quality command line argument
 - [ ] List channels you follow that are live
 - [ ] Allow change of channel
 - [ ] Allow change of stream quality (defaults to the best available right now)
--- slicktwitch.sh
+++ slicktwitch.sh
@@ -8,51 +8,167 @@
 #
 ################################
 
-configdir=$HOME/.config/slicktwitch
-configfile=$HOME/.config/slicktwitch/slicktwitch.conf
 
-if [ ! -d "$configdir" ]; then
-	mkdir "$configdir"
+# Handle flags
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            echo "slicktwitch -  Twitch stream and chat in the terminal"
+            echo " "
+            echo "slicktwitch [options]"
+            echo " "
+            echo "options:"
+            echo "-h, --help                                         show script help"
+            echo "-c CHANNEL, --CHANNEL=CHANNEL                      set twitch chat CHANNEL"
+            echo "-n NICK, --nick=NICK                               set twitch chat nick"
+            echo "-x, --nochat                                       do not open chat"
+            echo "-q QUALITY, --quality={best|worst|audio_only}     set preffered stream quality"
+            exit 0
+            ;;
+        -c)
+            shift
+            if test $# -gt 0; then
+                export CHANNEL=$1
+            else
+                echo "No CHANNEL specified"
+                exit 1
+            fi
+            shift
+            ;;
+        --CHANNEL*)
+            export CHANNEL=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
+            ;;
+        -n)
+            shift
+            if test $# -gt 0; then
+                export NICK=$1
+            else
+                echo "No NICK specified"
+                exit 1
+            fi
+            shift
+            ;;
+        --nick*)
+            export NICK=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
+            ;;
+        -q)
+            shift
+            if test $# -gt 0; then
+                export QUALITY=$1
+            else
+                echo "No QUALITY specified"
+                exit 1
+            fi
+            shift
+            ;;
+        --quality*)
+            export QUALITY=`echo $1 | sed -e 's/^[^=]*//g'`
+            shift
+            ;;
+        -x)
+            export NOCHAT=1
+            shift
+            ;;
+        --nochat)
+            export NOCHAT=1
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done 
+
+# Setup the config file
+CONFIGDIR=$HOME/.config/slicktwitch
+CONFIGFILE=$HOME/.config/slicktwitch/slicktwitch.conf
+
+if [ ! -d "$CONFIGDIR" ]; then
+    mkdir "$CONFIGDIR"
 fi
 
-if [ ! -f "$configfile" ]; then
-	echo "We need a Twitch OAuth key. You can generate one at https://twitchapps.com/tmi/"
-	echo -n "> "
-	read oauth
-	
-	if [ -z $oauth ]; then
-		echo "You need to pass a valid OAut key..."
-		exit 1
-	else
-		echo "$oauth" > "$configfile"
-	fi
+if [ ! -f "$CONFIGFILE" ]; then
+    echo "We need a Twitch OAuth key. You can generate one at https://twitchapps.com/tmi/"
+    echo -n "> "
+    read OAUTH
+    
+    if [ -z $OAUTH ]; then
+        echo "You need to pass a valid OAuth key..."
+        exit 1
+    else
+        echo "$OAUTH" > "$CONFIGFILE"
+    fi
 
-else oauth=`cat $configfile`
+else OAUTH=`cat $CONFIGFILE`
 
 fi
 
-echo "What channel would you like to watch today?"
-read channel
 
-if [ -z "$channel" ]; then
-	echo "You need to give a channel name..."
-	exit 1
+# Finish setting up arguments
+if [ -z "$CHANNEL" ]; then
+    echo "What CHANNEL would you like to watch today?"
+    read CHANNEL
+
+    if [ -z "$CHANNEL" ]; then
+        echo "You need to give a CHANNEL name..."
+        exit 1
+    fi
+    echo " "
 fi
 
-echo "Do you want to joing chat? If so what is your Twitch username? [Default: No]"
-echo -n "> "
-read nick
-
-echo "Attempting to connect to $channel..."
-streamlink -Q -p "vlc --qt-minimal-view" http://twitch.tv/$channel medium,worst,best &
-
-if [ ! -z "$nick" ]; then
-	echo "\n\n"
-	echo "IRSSI will open but you will need to join the chat channel yourself."
-	echo "\n\n"
-	echo "To join the chat type: /j $channel"
-	echo "\n\n"
-	echo -n "Press ENTER when ready."
-	read input
-	irssi -c irc.chat.twitch.tv -p 6667 -n $nick -w "$oauth"
+if [ -z "$NOCHAT" ]; then
+    if [ -z "$NICK" ]; then
+        echo "Do you want to joing chat? If so what is your Twitch username? [Default: No]"
+        echo -n "> "
+        read NICK
+        if [ -z "$NICK" ]; then
+            NOCHAT=1
+        fi
+    fi
+    echo " "
 fi
+
+if [ -z "$QUALITY" ]; then
+    echo "What quality do you want to watch the stream at (preferrably)?"
+    echo "1 - Best"
+    echo "2 - Worst"
+    echo "3 - Audio Only"
+    echo "[Default: 1]"
+    read QUALITY
+     QUALITY=${QUALITY:-1}
+    if [ "$QUALITY" -eq 1 ]; then
+        QUALITY='best'
+    elif [ "$QUALITY" -eq 2 ]; then
+        QUALITY='worst'
+    else
+        QUALITY='audio_only'
+    fi
+    echo " " 
+fi
+    
+
+# Launch programs...
+echo "**********************************************************"
+echo "Attempting to connect to $CHANNEL... this may take a while"
+echo "**********************************************************"
+
+if [ ! -z "$NOCHAT" ]; then
+    streamlink -p "vlc --qt-minimal-view" http://twitch.tv/$CHANNEL $QUALITY
+else
+    streamlink -Q -p "vlc --qt-minimal-view" http://twitch.tv/$CHANNEL $QUALITY &
+    echo ""
+    echo ""
+    echo "IRSSI will open but you will need to join the chat CHANNEL yourself."
+    echo ""
+    echo "To join the chat type: /j $CHANNEL"
+    echo ""
+    echo -n "Press ENTER when ready."
+    read input
+    irssi -c irc.chat.twitch.tv -p 6667 -n $NICK -w "$OAUTH"
+fi
+
+exit 0
+
+
